00375cad213d7d4c558f30e044c5247750f4f8ea
Sergio Torres <sdtorresl@unal.edu.co>
2019-10-01 03:19:46 -0500

    Player finished

--- js/main.js
+++ js/main.js
@@ -1,112 +1,189 @@
-// Setup player
-(function($) {
-    $(document).ready(function() {
+var artist, title, previousArtist, previousTitle;
 
-        const source = "https://streams.exeamedia.com/papa";
-        const audio = new Audio(); // use the constructor in JavaScript, just easier that way
-        audio.src = source;
-        audio.autoplay = true;
-        
+/** Make an http request and return the
+ *  results in a string
+ */
+function httpGet(URL) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", URL, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
 
-        const stream = {
-                title: "Papa Johns",
-                mp3: "https://streams.exeamedia.com/papa"
-            };
-        let ready = false;
-        const flagAudio = false;
+/** Update metadata of actual song
+ */
+function updateMetadata() {
 
+    data = 'tag=getTags';
+    $.ajax({
+        data: data,
+        url: 'metadata.php',
+        success: function(response) {
+            var json = JSON.parse(response);
 
-        $("#player").jPlayer({
-            ready: function (event) {
-                ready = true;
-                $(this).jPlayer("setMedia", stream);
-            },
-            pause: function() {
-                $(this).jPlayer("clearMedia");
-            },
-            error: function(event) {
-                if(ready && event.jPlayer.error.type === $.jPlayer.error.URL_NOT_SET) {
-                    // Setup the media stream again and play it.
-                    $(this).jPlayer("setMedia", stream).jPlayer("play");
+            if (json[0] != artist && json[1] != title) {
+                previousArtist = artist;
+
+                previousTitle = title;
+
+                artist = json[0];
+                if (artist.length > 23) {
+                    artist;
                 }
-            },
-            swfPath: "js",
-            supplied: "mp3",
-            preload: "none",
-            wmode: "window",
-            keyEnabled: false,
-            cssSelectorAncestor: ".player-container",
-            autoPlay: true,
-            verticalVolume: "height"
-        });
 
-        setInterval(updateMetadata(), 5000);
-    });
+                title = json[1];
+                if (artist.length > 23) {
+                    title;
+                }
 
-    $(window).load(function(){
-        // $('#player').jPlayer("play");
-        // Get info of the current track
-        setInterval("updateMetadata()", 20000);
-    });
+                $("#artist").text(artist);
+                $("#title").text(title);
 
-    /** Make an http request and return the
-     *  results in a string
-     */
-    function httpGet(URL) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", URL, false);
-        xmlHttp.send(null);
-        return xmlHttp.responseText;
-    }
-
-    var artist, title, previousArtist, previousTitle;
-
-    /** Update metadata of actual song
-     */
-    function updateMetadata() {
-
-        data = 'tag=getTags';
-        $.ajax({
-            data: data,
-            url: 'metadata.php',
-            success: function(response) {
-                var json = JSON.parse(response);
-
-                if (json[0] != artist && json[1] != title) {
-                    previousArtist = artist;
-
-                    previousTitle = title;
-
-                    artist = json[0];
-                    if (artist.length > 23) {
-                        artist;
-                    }
-
-                    title = json[1];
-                    if (artist.length > 23) {
-                        title;
-                    }
-
-                    $("#artist").text(artist);
-                    $("#title").text(title);
-
-                    if (previousArtist != undefined) {
-                        $("#previousSong").html(previousArtist + ' - ' + previousTitle);
-                    }
+                if (previousArtist != undefined) {
+                    $("#previousSong").html(previousArtist + ' - ' + previousTitle);
                 }
             }
-        });
+        }
+    });
+}
+
+function toHHMMSS(seconds) {
+    var sec_num = parseInt(seconds, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    return hours+':'+minutes+':'+seconds;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Setup player
+    const source = "https://streams.exeamedia.com/papa";
+
+    const player = new Audio();
+    player.src = source;
+    player.autoplay = false;
+    player.volume = 0.7;
+    
+    btnPlayPause = document.getElementById('btn-play-pause');
+    btnMute = document.getElementById('btn-mute');
+    progressBar = document.getElementById('progress-bar');
+    volumeBar = document.getElementById('volume-bar');
+    volumeValue = document.getElementById('volume-value');
+    btnVolumeUp = document.getElementById('volume-up');
+    btnVolumeDown = document.getElementById('volume-down');
+    elapsedTime = document.getElementById('elapsed-time');
+
+    // Update the video volume
+    volumeBar.addEventListener("change", function(evt) {
+        function displayMessage(message, canPlay) {
+            var element = document.querySelector('#message');
+            element.innerHTML = message;
+            element.className = canPlay ? 'info' : 'error';
+        }
+        player.volume = parseInt(evt.target.value) / 10;
+    });
+
+    // Add a listener for the timeupdate event so we can update the progress bar
+    player.addEventListener('timeupdate', updateProgressBar, false);
+
+    // Add a listener for the play and pause events so the buttons state can be updated
+    player.addEventListener('play', function() {
+        // Change the button to be a pause button
+        changeButtonType(btnPlayPause, 'pause');
+    }, false);
+
+    player.addEventListener('pause', function() {
+        // Change the button to be a play button
+        changeButtonType(btnPlayPause, 'play');
+    }, false);
+
+    player.addEventListener('ended', function() {
+        this.pause();
+    }, false);
+
+    progressBar.addEventListener("click", seek);
+
+    btnPlayPause.addEventListener("click", playPauseAudio);
+
+    btnVolumeUp.addEventListener("click", function () {
+        if (player.src) {
+            if ((player.volume + 0.1) <= 1) {
+                player.volume = player.volume + 0.1;
+            } else {
+                player.volume = 1;
+            }
+        }
+    });
+
+    btnVolumeDown.addEventListener("click", function () {
+        if (player.src) {
+            if ((player.volume-0.1) >= 0) {
+                player.volume = player.volume - 0.1;
+            } else {
+                player.volume = 0;
+            }
+        }
+    });
+
+    player.addEventListener('volumechange', function(e) { 
+        volumeValue.style.width = player.volume * 100 + '%';
+    }, false);	
+
+    function seek(e) {
+        if (player.src) {
+            var percent = e.offsetX / this.offsetWidth;
+            player.currentTime = percent * player.duration;
+            e.target.value = Math.floor(percent / 100);
+            e.target.innerHTML = progressBar.value + '% played';
+        }
     }
 
-    $('.backbtn').click(function () {
-        console.log('hola');
-        $('.widget').addClass('hidden');
-        $('#content-slow').show();
-    });
+    function playPauseAudio() {
+        if (player.src) {
+            if (player.paused || player.ended) {
+                // Change the button to a pause button
+                changeButtonType(btnPlayPause, 'pause');
+                player.play();
+            } else {
+                // Change the button to a play button
+                changeButtonType(btnPlayPause, 'play');
+                player.pause();
+            }
 
-    $('.close').click(function () {
-        $('#content-slow').hide();
-    });
+            volumeValue.style.width = player.volume * 100 + '%';
+        }
+    }
 
-    window.updateMetadata = updateMetadata;
-})(jQuery);
+    // Stop the current media from playing, and return it to the start position
+    function stopAudio() {
+        if (player.src) {
+            player.pause();
+            if (player.currentTime) player.currentTime = 0;
+        }
+    }
+
+    // Update the progress bar
+    function updateProgressBar() {
+        // Work out how much of the media has played via the duration and currentTime parameters
+        var percentage = Math.floor((100 / player.duration) * player.currentTime);
+        // Update the progress bar's value
+        progressBar.value = percentage;
+
+        elapsedTime.innerHTML = toHHMMSS(player.currentTime);
+    }
+
+    // Updates a button's title, innerHTML and CSS class
+    function changeButtonType(btn, value) {
+        if (value === "pause") {
+            btn.className = "pause";
+        } else {
+            btn.className = "play";
+        }
+    }
+
+    setInterval(updateMetadata(), 5000);
+});
